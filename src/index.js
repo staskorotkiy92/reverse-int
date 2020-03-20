c43a7b75dceb02455ad19af056ff5bda40fb5b6f
staskorotkiy92 <staskorotkiy92@gmail.com>
2020-03-21 00:22:22 +0300

    complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n>0)
+  return +n.toString().split('').reverse().join('');
+
+  return  (+Math.abs(n).toString().split('').reverse().join(''));
+
 }
+
